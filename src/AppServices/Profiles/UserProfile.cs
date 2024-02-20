4ed7d385e660ce4f4f5e43f06eeec31c91478bef
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-20 01:04:16 -0300

    feat: criacao de profile automapper para user

---
+++ src/AppServices/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Models;
+
+namespace AppServices.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserResult>();
+            CreateMap<UpdateUserRequest, User>();
+            CreateMap<CreateUserRequest, User>();
+        }
+    }
+}
