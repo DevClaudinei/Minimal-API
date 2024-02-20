b7fb89b28c76a9132238695ddc9019b0a73ec01c
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-20 01:03:30 -0300

    feat: criacao de DTOs

---
+++ src/Application.Models/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace AppServices;
+
+public class CreateUserRequest
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+    public string? Email { get; set; }
+    public string? Nickname { get; set; }
+}
---
+++ src/Application.Models/UserResult.cs
@@ -0,0 +1,13 @@
+﻿namespace AppServices
+{
+    public class UserResult
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+        public string? Email { get; set; }
+        public string? Nickname { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
