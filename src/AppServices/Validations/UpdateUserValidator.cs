ccb5da9f931069862744fd7fd655b39db89bdfd4
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-21 23:14:16 -0300

    feat|refactor: criacao de validacao para DTOs

---
+++ src/AppServices/Validations/UpdateUserValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace AppServices.Validations
+{
+    public class UpdateUserValidator: AbstractValidator<UpdateUserRequest>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .NotNull()
+                .Must(x => x?.Split(" ").Length >= 1)
+                .WithMessage("Lastname deve conter ao menos um sobrenome");
+
+            RuleFor(x => x.Age)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .EmailAddress();
+
+            RuleFor(x => x.NickName)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3);
+        }
+    }
+}
